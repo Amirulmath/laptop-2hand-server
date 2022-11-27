c45203a5a2aac19910ec114df59947bc4b6cbac6
Amirulmath <amirulmath760@gmail.com>
2022-11-27 21:14:34 +0600

    categories API receive fron server

--- index.js
+++ index.js
@@ -13,12 +13,24 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xjhllfd.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run() {
+    try {
+        const categoriesCollection = client.db('laptopShop').collection('categories');
+
+        // Use Aggregate to query multiple collection and then merge data
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const category = await categoriesCollection.find(query).toArray();
+            res.send(category);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
 
 app.get('/', async (req, res) => {
     res.send('Laptop2Hand server is running');
