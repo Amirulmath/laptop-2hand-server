62674deb3b5f78b3b57dbc531a22d75e0e1ee2d6
Amirulmath <amirulmath760@gmail.com>
2022-11-29 11:30:42 +0600

    My Product API create & send

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -26,6 +26,26 @@ async function run() {
             res.send(category);
         });
 
+        app.get('/myproduct', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const myProduct = await productsCollection.find(query).toArray();
+            res.send(myProduct);
+        })
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const products = await productsCollection.find(query).toArray();
+            res.send(products);
+        });
+
+        // app.get('/products/:brand', async (req, res) => {
+        //     const brand = req.params.brand;
+        //     const query = {brand: ObjectId(brand)};
+        //     const productsBrand = await productsCollection.filter(query);
+        //     res.send(productsBrand);
+        // });
+        
         app.post('/products', async(req, res) =>{
             const product = req.body;
             const result = await productsCollection.insertOne(product);
